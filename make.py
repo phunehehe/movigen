8b5376a01ef9dc17b493e852a0ce03a3bc5f1e1f
Hoang Xuan Phu <phunehehe@gmail.com>
2013-04-18 22:31:12 +0700

    fancy stuff (not likely to work)

--- make.py
+++ make.py
@@ -2,58 +2,59 @@
 # coding=utf-8
 
 import re
-import os
 
+from os import walk
 from os.path import basename, dirname, join
 
 
+index_base = 'index.html'
+matches = {
+#   '/root/grandparent': {
+#       'parent': [
+#           'file1',
+#           'file2',
+#       ]
+#   }
+}
 regex = re.compile('.*\.(mkv|mp4|avi)$')
-matches = {}
-
-for root, dirs, files in os.walk('files', followlinks=True):
-    for name in files:
-        if regex.match(name):
-            f = os.path.join(root, name)
-            d = dirname(dirname(f))
-            try:
-                matches[d].append(f)
-            except KeyError:
-                matches[d] = [f]
 
 
-for d, files in matches.items():
+with open('templates/header.html') as header_file:
+    header_template = header_file.read()
 
-    with open('templates/header.html') as header_file:
-        header = header_file.read() % {
-            'set_name': basename(d)
-        }
+with open('templates/piece.html') as piece_file:
+    piece_template = piece_file.read()
 
-    with open('templates/piece.html') as piece_file:
-        piece_template = piece_file.read()
+with open('templates/footer.html') as footer_file:
+    footer = footer_file.read()
 
-    with open('templates/footer.html') as footer_file:
-        footer = footer_file.read()
+
+def add_match(child_full):
+
+    child = basename(child_full)
+    parent_full = dirname(child_full)
+
+    parent = basename(parent_full)
+    grandparent = dirname(parent_full)
+
+    if grandparent not in matches:
+        matches[grandparent] = {parent: [child]}
+    else:
+        mg = matches[grandparent]
+        if parent not in mg:
+            mg[parent] = [child]
+        else:
+            mg[parent].append(child)
+
+
+def generate_series_content(grandparent, parent, children):
 
     content = ''
 
-    for f in sorted(files):
+    for child in children:
 
-        # Count the number of movie files in the same dir. If there are more
-        # than one, we are dealing with a series
-        parent_dir = dirname(f)
-        count = 0
-        is_series = False
-        for name in os.listdir(parent_dir):
-            if regex.match(name):
-                count += 1
-                if count > 1:
-                    is_series = True
-                    break
-
-        parent_name = basename(parent_dir)
-        f_name = basename(f)
-        file_path = join(parent_name, f_name)
-        movie_name = '%s, %s' % (parent_name, f_name) if is_series else parent_name
+        movie_name = '%s, %s' % (parent, child)
+        file_path = child
 
         content += piece_template % {
             'movie_name': movie_name,
@@ -62,7 +63,54 @@ for d, files in matches.items():
             'thumbnail_path': './%s.jpg' % file_path,
         }
 
-    index_fname = '%s/index.html' % d
+    return content
+
+
+def generate_single_content(grandparent, parents):
+
+    content = ''
+
+    for parent, children in parents.items():
+        # Assumming there is only one child
+        child = children[0]
+
+        file_path = join(parent, child)
+
+        content += piece_template % {
+            'movie_name': parent,
+            'movie_path': file_path,
+            'subtitle_path': './%s-vi.srt' % file_path,
+            'thumbnail_path': './%s.jpg' % file_path,
+        }
+
+    return content
+
+
+for root, dirs, files in walk('files', followlinks=True):
+    for name in files:
+        if regex.match(name):
+            add_match(join(root, name))
+
+
+for grandparent, parents in matches.items():
+
+    for parent, children in parents.items():
+
+        if len(children) > 1:
+            is_series = True
+            set_name = parent
+            index_fname = '%s/%s' % (join(grandparent, parent), index_base)
+            content = generate_series_content(grandparent, parent, children)
+        else:
+            is_series = False
+            movie_name = parent
+            set_name = basename(grandparent)
+            index_fname = '%s/%s' % (grandparent, index_base)
+            content = generate_single_content(grandparent, parents)
+
+        header = header_template % {
+            'set_name': set_name,
+        }
 
     with open(index_fname, 'w') as index_file:
         index_file.write(header)
