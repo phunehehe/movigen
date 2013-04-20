3e080e32ba899592614d77daa257ee464904489b
Hoang Xuan Phu <phunehehe@gmail.com>
2013-04-21 00:10:32 +0700

    check subtitle exists, again

--- make.py
+++ make.py
@@ -92,10 +92,14 @@ def generate_single_content(grandparent, parents):
         child = children[0]
         file_path = join(parent, child)
 
+        subtitle_path = './%s-vi.srt' % file_path
+        if not isfile(subtitle_path):
+            subtitle_path = ''
+
         content += piece_template % {
             'movie_name': parent,
             'movie_path': file_path,
-            'subtitle_path': './%s-vi.srt' % file_path,
+            'subtitle_path': subtitle_path,
             'thumbnail_path': './%s.jpg' % file_path,
         }
 
