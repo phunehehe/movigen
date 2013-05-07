d58fa61d6e2e31dfc9c0803b12f79b4c65ec5953
Hoang Xuan Phu <phunehehe@gmail.com>
2013-05-07 22:53:57 +0700

    should be working now, but somehow it is not

--- js/movies.js
+++ js/movies.js
@@ -1,39 +1,55 @@
 // Hide empty links (missing subtitle)
 $('a[href=""]').hide();
 
-// IntentBridge for Android
+
+// IntentBridge
+
+
+function makeLink(action, target, extras) {
+    return [
+        'http://phunehehe.github.io/IntentBridge',
+        action,
+        encodeURIComponent(target),
+        encodeURIComponent(JSON.stringify(extras))
+    ].join('/');
+}
+
+
 //if (navigator.userAgent.indexOf('Android') > 0) {
 if (true) {
+
     $('#movies > .movie').each(function() {
 
-        var movie_element = $(this);
-        var subtitle_element = movie_element.find('.subtitle_path');
-        var subtitle_path = subtitle_element.attr('href');
-        if (!subtitle_path) {
+        var movieElement = $(this);
+        var subtitleElement = movieElement.find('.subtitlePath');
+        var subtitlePath = subtitleElement.attr('href');
+        if (!subtitlePath) {
             // No subtitle, no magic
             return;
         }
 
-        subtitle_path = subtitle_element[0].href;
-        var movie_path = movie_element.find('.movie_path')[0].href;
-        var extras = JSON.stringify([
+        subtitlePath = subtitleElement[0].href;
+        var moviePath = movieElement.find('.moviePath')[0].href;
+
+        var extras = [
             {
-                "name": "subs",
-                "type": "android.net.Uri[]",
-                "value": [subtitle_path]
+                'name': 'subs',
+                'type': 'android.net.Uri[]',
+                'value': [subtitlePath]
             }
-        ]);
-        var le_url = 'http://phunehehe.github.io/IntentBridge/android.intent.action.VIEW/' +
-                     encodeURIComponent(movie_path) +
-                     '/' +
-                     encodeURIComponent(extras);
-        var links_element = movie_element.find('.links')[0];
+        ];
+
+        var leUrl = makeLink('android.intent.action.VIEW',
+                             moviePath,
+                             extras);
+
+        var linkElement = movieElement.find('.links')[0];
 
         // Replace the subtitle link with IntentBridge
-        subtitle_element.hide();
+        subtitleElement.hide();
         $('<a/>', {
-            href: le_url,
+            href: leUrl,
             text: 'Xem!'
-        }).appendTo(links_element);
+        }).appendTo(linkElement);
     });
 }
